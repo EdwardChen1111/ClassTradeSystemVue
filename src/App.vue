a3c382f776bb5b52f0ee8b7c77714b88e248e4b3
EdwardChen1111 <asus960324@gmail.com>
2024-04-22 02:02:34 +0800

    alpha1.1.0

--- src/App.vue
+++ src/App.vue
@@ -5,49 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=10.0, user-scalable=yes">
   </head>
   <body>
-    <header>
-      <button id="asidebtn"><img src="./asidebtn.png"></button>
+    <header id="header">
+      <button id="asidebtn" @click.self="ocaside"><img src="./asidebtn.png" @click.self="ocaside"></button>
       <span id="headerindex">校慶班級收銀系統</span>
-      <button class="headerbtn arrowbtn" id="cashbtn" v-on:click="change"><span>收銀</span></button>
-      <button class="headerbtn arrowbtn" id="mdishbtn" v-on:click="change"><span>製餐</span></button>
-      <button class="headerbtn arrowbtn" id="odishbtn" v-on:click="change"><span>出餐</span></button>
-      <button class="headerbtn arrowbtn" id="backbtn" v-on:click="change"><span>後台</span></button>
+      <button class="headerbtn arrowbtn" id="cashbtn" @click="change('cashbtn')"><span @click="change('cashbtn')">收銀</span></button>
+      <button class="headerbtn arrowbtn" id="mdishbtn" @click="change('mdishbtn')"><span @click="change('mdishbtn')">製餐</span></button>
+      <button class="headerbtn arrowbtn" id="odishbtn" @click="change('odishbtn')"><span @click="change('odishbtn')">出餐</span></button>
+      <button class="headerbtn arrowbtn" id="backbtn" @click="change('backbtn')"><span @click="change('backbtn')">後台</span></button>
     </header>
-    <main id="mainsystem">
-      <div class="cash">
-        <h1>這是收銀</h1>
-        <table>
-          <caption>訂單資料</caption>
-          <thead>
-            <tr>
-              <td>訂單編號</td>
-              <td>詳細資料</td>
-              <td>訂單價格</td>
-              <td>等待時間</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>#000</td>
-              <td>
-                <button>查閱詳細項目</button>
-              </td>
-              <td>1000</td>
-              <td>5min</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="mdish">
-        <h1>這是製餐</h1>
-      </div>
-      <div class="odish">
-        <h1>這是出餐</h1>
-      </div>
-      <div class="back">
-        <h1>這是後台</h1>
-      </div>
-    </main>
     <main id="loginsystem">
       <form @submit.prevent="login" id="loginform">
         <legend>校慶班級收銀系統登入</legend>
@@ -71,14 +36,85 @@
         </p>
       </form>
     </main>
+    <main id="mainsystem">
+      <aside v-html="aside"></aside>
+      <div id="cash">
+        <table>
+          <caption>訂單資料</caption>
+          <thead>
+            <tr>
+              <td>訂單編號</td>
+              <td>詳細資料</td>
+              <td>訂單價格</td>
+              <td>等待時間</td>
+              <td>移除訂單</td>
+            </tr>
+          </thead>
+          <tbody v-html="cashlist"></tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <button>新增訂單</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div id="mdish">
+        <table>
+          <caption>{{mdishtype}}餐製作列表</caption>
+          <thead>
+            <tr>
+              <td>製作份數</td>
+              <td>製作時間</td>
+              <td>開始製作</td>
+              <td>暫停製作</td>
+              <td>完成製作</td>
+            </tr>
+          </thead>
+          <tbody v-html="mdishlist"></tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <button>重新整理</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div id="odish">
+        <table>
+          <caption>訂單資料</caption>
+          <thead>
+            <tr>
+              <td>訂單編號</td>
+              <td>詳細資料</td>
+              <td>等待時間</td>
+            </tr>
+          </thead>
+          <tbody v-html="odishlist"></tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <button>重新整理</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div id="back">
+        <h1>這是後台</h1>
+      </div>
+    </main>
   </body>
 </template>
 
 <script startup>
   import "./style.css"
+  import "./main.js"
 
   export default {
-    data() {
+    data () {
       return {
         loginstatus: 'flex',
         wronglogin: 'none',
@@ -93,10 +129,37 @@
         mdishstatus: 'none',
         odishstatus: 'none',
         backstatus: 'none',
+
+        aside: '<p>1111</p>',
+        asidestatus: 'none',
+        asidetype: false,
+
+        cashthing: {
+          "num": ['#000'],
+          "price": [1000],
+          "time": [5],
+          "order": [0]
+        },
+        cashlist: '',
+
+        mdishtype: 'yeeeeee',
+        mdishthing: {
+          "num": [1],
+          "time": [5],
+          "order": [0],
+        },
+        mdishlist: '',
+
+        odishthing: {
+          "num": ['#000'],
+          "time": [5],
+          "order": [0]
+        },
+        odishlist: '',
       }
     },
     methods: {
-      login() {
+      login () {
         if (this.passw === this.account) {
           this.systemstatus = 'flex';
           this.loginstatus = 'none';
@@ -115,16 +178,67 @@
           this.wronglogin = 'flex';
         }
       },
-      change(event){
+      change (name) {
         this.cashstatus = this.mdishstatus = this.odishstatus = this.backstatus = 'none';
-        if (event.target.id === 'cashbtn') {
+        this.ocaside();
+        if (name == 'cashbtn') {
           this.cashstatus = 'flex';
-        } else if (event.target.id === 'mdishbtn') {
+          this.cashlist =
+            `<tr>\
+              <td>${this.cashthing["num"][0] == undefined ? 'none' : this.cashthing["num"][0]}</td>\
+              <td><button @click="detail(${this.cashthing["order"][0] == undefined ? 'none' : this.cashthing["order"][0]})">詳細內容</button></td>\
+              <td>${this.cashthing["price"][0] == undefined ? 'none' : this.cashthing["price"][0]}元</td>\
+              <td>${this.cashthing["time"][0] == undefined ? 'none' : this.cashthing["time"][0]}min</td>\
+              <td><button @click="remove(${this.cashthing["order"][0] == undefined ? 'none' : this.cashthing["order"][0]})">取消訂單</button></td>\
+            </tr>`;
+        } else if (name == 'mdishbtn') {
           this.mdishstatus = 'flex';
-        } else if (event.target.id === 'odishbtn') {
+          this.mdishlist =
+            `<tr>\
+              <td>${this.mdishthing["num"][0] == undefined ? 'none' : this.mdishthing["num"][0]}</td>\
+              <td>${this.mdishthing["time"][0] == undefined ? 'none' : this.mdishthing["time"][0]}min</td>\
+              <td><button @click="start(${this.mdishthing["order"][0] == undefined ? 'none' : this.mdishthing["order"][0]})">開始製作</button></td>\
+              <td><button @click="stop(${this.mdishthing["order"][0] == undefined ? 'none' : this.mdishthing["order"][0]})">暫停製作</button></td>\
+              <td><button @click="finish(${this.mdishthing["order"][0] == undefined ? 'none' : this.mdishthing["order"][0]})">完成製作</button></td>\
+            </tr>`;
+        } else if (name == 'odishbtn') {
           this.odishstatus = 'flex';
-        } else {
+          this.odishlist =
+            `<tr>\
+              <td>${this.cashthing["num"][0] == undefined ? 'none' : this.cashthing["num"][0]}</td>\
+              <td><button @click="detail(${this.cashthing["order"][0] == undefined ? 'none' : this.cashthing["order"][0]})">詳細內容</button></td>\
+              <td>${this.cashthing["time"][0] == undefined ? 'none' : this.cashthing["time"][0]}min</td>\
+            </tr>`;
+        } else if (name == 'backbtn') {
           this.backstatus = 'flex';
+        }
+      },
+      ocaside () {
+        if (this.asidetype) {
+          this.asidetype = false;
+          this.asidestatus = 'none';
+        } else {
+          this.asidetype = true;
+          this.asidestatus = 'flex';
+          if (this.mdishstatus == 'flex') {
+            this.aside =
+              `<button>a餐</button>
+              <button>b餐</button>
+              <button>c餐</button>
+              <button>d餐</button>
+              `;
+          } else if (this.backstatus == 'flex') {
+            this.aside =
+              `<button>統計</button>
+              <button>售價</button>
+              <button>折價</button>
+              <button>公式</button>
+              `;
+          } else {
+            this.aside =
+              `<h1>這裡什麼都沒有</h1>
+              `;
+          }
         }
       }
     }
@@ -134,6 +248,10 @@
 <style>
   header {
     display: v-bind(systemstatus);
+  }
+
+  aside {
+    display: v-bind(asidestatus);
   }
 
   #cashbtn {
@@ -164,19 +282,19 @@
     display: v-bind(systemstatus);
   }
 
-  .cash {
+  #cash {
     display: v-bind(cashstatus);
   }
 
-  .mdish {
+  #mdish {
     display: v-bind(mdishstatus);
   }
 
-  .odish {
+  #odish {
     display: v-bind(odishstatus);
   }
 
-  .back {
+  #back {
     display: v-bind(backstatus);
   }
 </style>
