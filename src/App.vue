d968e938d7cf25b5042fb666711a3162a330dc61
EdwardChen1111 <asus960324@gmail.com>
2024-04-28 00:21:06 +0800

    alpha1.4.0

--- src/App.vue
+++ src/App.vue
@@ -5,50 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=10.0, user-scalable=yes">
   </head>
   <body>
-    <header id="header" v-if="SystemStatus">
-      <button id="asidebtn" @click.self="ocaside"><img src="./asidebtn.png" @click.self="ocaside"></button>
-      <span id="headerindex">校慶班級收銀系統</span>
-      <button class="headerbtn arrowbtn" id="cashbtn" v-if="HeaderBtnStatus.cashbtn" @click="ChangePage('cash')"><span @click="ChangePage('cash')">收銀</span></button>
-      <button class="headerbtn arrowbtn" id="mdishbtn" v-if="HeaderBtnStatus.mdishbtn" @click="ChangePage('mdish')"><span @click="ChangePage('mdish')">製餐</span></button>
-      <button class="headerbtn arrowbtn" id="odishbtn" v-if="HeaderBtnStatus.odishbtn" @click="ChangePage('odish')"><span @click="ChangePage('odish')">出餐</span></button>
-      <button class="headerbtn arrowbtn" id="backbtn" v-if="HeaderBtnStatus.backbtn" @click="ChangePage('back')"><span @click="ChangePage('back')">後台</span></button>
-    </header>
-    <main id="loginsystem" v-if="LoginStatus">
-      <form @submit.prevent="login" id="loginform">
-        <legend>校慶班級收銀系統登入</legend>
-        <p id="account">
-          <label id="labela">帳號</label>
-          <select size="0" name="account" id="acselect" v-model="account">
-            <option value="none" selected disabled>請選擇職位</option>
-            <option value="cash">收銀</option>
-            <option value="mdish">製餐</option>
-            <option value="odish">出餐</option>
-            <option value="back">後台</option>
-          </select>
-        </p>
-        <p id="password">
-          <label id="labelp">密碼</label>
-          <input type="password" name="passw" id="passw" v-model="passw" required>
-        </p>
-        <p id="submit">
-          <button type="submit" class="arrowbtn" id="loginbtn"><span>登入</span></button>
-          <span id="wronglogin" v-if="WrongLogin">帳號或密碼錯誤</span>
-        </p>
-      </form>
-    </main>
-    <main id="mainsystem" v-if="SystemStatus">
-      <aside>
-        <h1 v-if="MainPageStatus.mdish === MainPageStatus.back">這裡什麼都沒有</h1>
-        <button class="asidelistbtn" v-if="MainPageStatus.mdish">a餐</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.mdish">b餐</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.mdish">c餐</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.mdish">d餐</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('statistics')">統計</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('mealsetting')">售價</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('formula')">公式</button>
-        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('setting')">設定</button>
-      </aside>
-      <dialog id="addorder" v-show="AddOrderStatus" @click="OCAddOrder">
+    <transition name="dialog" @after-leave="OCAddOrder(false)">
+      <dialog ref="AddOrder" v-show="AddOrderStatus" id="addorder" @click="AOHCOS">
         <table>
           <thead>
             <tr>
@@ -73,20 +31,22 @@
               <span>找零{{ordering.hpay}}元</span>
             </div>
             <div>
-              <button @click="OCAddOrder">取消</button>
+              <button @click="AddOrderStatus = false">取消</button>
               <p>
-                <span>需等待{{ordering.time}}分鐘</span>
+                <span>需等待{{CalTime(ordering.time)}}分鐘</span>
                 <button>送出</button>
               </p>
             </div>
           </tfoot>
         </table>
       </dialog>
-      <dialog id="detail" v-if="DetailStatus" @click="OCDetail">
+    </transition>
+    <transition name="dialog" @after-leave="OCDetail(false)">
+      <dialog ref="Detail" id="detail" v-show="DetailStatus" @click="DHCOS">
         <table>
           <caption>
-            <span>單號:{{number}}</span>
-            <span>需等待{{time}}</span>
+            <span>單號:1<!--{{number}}--></span>
+            <span>需等待1<!--{{time}}--></span>
           </caption>
           <thead>
             <tr>
@@ -101,11 +61,55 @@
             </tr>
           </tbody>
           <tfoot>
-            <button @click="OCDetail">完成</button>
-            <span>共{{total}}項</span>
+            <button @click="DetailStatus = false">完成</button>
+            <span>共1<!--{{total}}-->項</span>
           </tfoot>
         </table>
       </dialog>
+    </transition>
+    <header id="header" v-if="SystemStatus">
+      <button id="asidebtn" @click.self="ocaside"><img src="./asidebtn.png" @click.self="ocaside"></button>
+      <span id="headerindex">校慶班級收銀系統</span>
+      <button class="headerbtn arrowbtn" id="cashbtn" v-if="HeaderBtnStatus.cashbtn" @click="ChangePage('cash')"><span @click="ChangePage('cash')">收銀</span></button>
+      <button class="headerbtn arrowbtn" id="mdishbtn" v-if="HeaderBtnStatus.mdishbtn" @click="ChangePage('mdish')"><span @click="ChangePage('mdish')">製餐</span></button>
+      <button class="headerbtn arrowbtn" id="odishbtn" v-if="HeaderBtnStatus.odishbtn" @click="ChangePage('odish')"><span @click="ChangePage('odish')">出餐</span></button>
+      <button class="headerbtn arrowbtn" id="backbtn" v-if="HeaderBtnStatus.backbtn" @click="ChangePage('back')"><span @click="ChangePage('back')">後台</span></button>
+    </header>
+    <main id="loginsystem" v-if="!SystemStatus">
+      <form @submit.prevent="login" id="loginform">
+        <legend>校慶班級收銀系統登入</legend>
+        <p id="account">
+          <label id="labela">帳號</label>
+          <select size="0" name="account" id="acselect" v-model="userinfo.account">
+            <option value="none" selected disabled>請選擇職位</option>
+            <option value="cash">收銀</option>
+            <option value="mdish">製餐</option>
+            <option value="odish">出餐</option>
+            <option value="back">後台</option>
+          </select>
+        </p>
+        <p id="password">
+          <label id="labelp">密碼</label>
+          <input type="password" name="passw" id="passw" v-model="userinfo.passw" required>
+        </p>
+        <p id="submit">
+          <button type="submit" class="arrowbtn" id="loginbtn"><span>登入</span></button>
+          <span id="wronglogin" v-if="WrongLogin">帳號或密碼錯誤</span>
+        </p>
+      </form>
+    </main>
+    <main id="mainsystem" v-if="SystemStatus">
+      <aside>
+        <h1 v-if="MainPageStatus.mdish === MainPageStatus.back">這裡什麼都沒有</h1>
+        <button class="asidelistbtn" v-if="MainPageStatus.mdish">a餐</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.mdish">b餐</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.mdish">c餐</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.mdish">d餐</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('statistics')">統計</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('mealsetting')">售價</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('formula')">公式</button>
+        <button class="asidelistbtn" v-if="MainPageStatus.back" @click="ChangeBackPage('setting')">設定</button>
+      </aside>
       <div id="cash" v-if="MainPageStatus.cash">
         <table>
           <caption>訂單資料</caption>
@@ -130,7 +134,7 @@
           <tfoot>
             <tr>
               <td>
-                <button @click="OCAddOrder">新增訂單</button>
+                <button @click="AddOrderStatus = true">新增訂單</button>
               </td>
             </tr>
           </tfoot>
@@ -304,9 +308,10 @@
   </body>
 </template>
 
-<script startup>
+<script setup>
   import "./style.css"
   import "./main.js"
+  import { ref, onMounted, watch } from "vue";
 
   let COD = [
     {
@@ -598,169 +603,236 @@
     }
   ];
 
-  export default {
-    data () {
-      return {
-        LoginStatus: true,
-        WrongLogin: false,
+  let SystemStatus = ref(false),
+    WrongLogin = ref(false),
+    userinfo = ref({
+      class: 201,
+      account: 'cash',
+      passw: ''
+    }),
 
-        SystemStatus: false,
-        AsideStatus: false,
-        AsidePos: '-9%',
+    AsideStatus = ref(false),
+    AsidePos = ref('-9%'),
         
-        HeaderBtnStatus: {
-          cashbtn: false,
-          mdishbtn: false,
-          odishbtn: false,
-          backbtn: false
-        },
+    HeaderBtnStatus = ref({
+      cashbtn: false,
+      mdishbtn: false,
+      odishbtn: false,
+      backbtn: false
+    }),
 
-        MainPageStatus: {
-          cash: false,
-          mdish: false,
-          odish: false,
-          back: false
-        },
+    MainPageStatus = ref({
+      cash: false,
+      mdish: false,
+      odish: false,
+      back: false
+    }),
 
-        BackPageStatus: {
-          statistics: true,
-          mealsetting: false,
-          formula: false,
-          setting: false
-        },
+    BackPageStatus = ref({
+      statistics: true,
+      mealsetting: false,
+      formula: false,
+      setting: false
+    }),
 
-        CashOdishData: COD,
-        MdishData: MDD,
-        StatisticsData: SD,
-        MealSettingData: MSD,
-        FormulaSettingData: FSD,
+    CashOdishData = ref(COD),
+    MdishData = ref(MDD),
+    StatisticsData = ref(SD),
+    MealSettingData = ref(MSD),
+    FormulaSettingData = ref(FSD),
 
-        MdishType: 'a餐',
+    MdishType = ref('a餐'),
 
-        AddOrderStatus: false,
-        DetailStatus: false,
+    DetailStatus = ref(false),
+    AddOrderStatus = ref(false),
 
-        ordering: {
-          data: [
-            {
-              type: 'a餐',
-              price: 1000,
-              count: 0
-            }
-          ],
-          npay: 0,
-          total: 0,
-          hpay: self.total - self.npay,
-          time: {
-            hour: 1,
-            min: 1,
-            sec: 1
-          }
+    AddOrder = ref(),
+    Detail = ref(),
+    
+    ordering = ref({
+      data: [
+        {
+          type: 'a餐',
+          price: 1000,
+          count: 0
         }
+      ],
+      npay: 0,
+      total: 0,
+      hpay: self.total - self.npay,
+      time: {
+        hour: 1,
+        min: 1,
+        sec: 1
       }
-    },
-    methods: {
-      login () {
-        if (this.passw === this.account) {
-          this.SystemStatus = true;
-          this.LoginStatus = false;
-          this.WrongLogin = false;
-          this.MainPageStatus[this.account] = true;
+    });
+    
+  function login () {
+    if (userinfo.value.passw == userinfo.value.account) {
+      SystemStatus.value = true;
+      WrongLogin.value = false;
+      MainPageStatus.value[userinfo.value.account] = true;
 
-          if (this.account === 'back') {
-            for (let i in this.HeaderBtnStatus) {
-              this.HeaderBtnStatus[i] = true;
-            }
-          } else {
-            this.HeaderBtnStatus[this.account + 'btn'] = true;
-          }
-        } else {
-          this.WrongLogin = true;
+      if (userinfo.value.account == 'back') {
+        for (let i in HeaderBtnStatus.value) {
+          HeaderBtnStatus.value[i] = true;
         }
-      },
-
-      ChangePage (name) {
-        for (let i in this.MainPageStatus) {
-          this.MainPageStatus[i] = false;
-        }
-
-        if (this.AsideStatus) {
-          this.ocaside();
-        }
-        
-        this.MainPageStatus[name] = true;
-      },
-
-      ChangeBackPage (name) {
-        for (let i in this.BackPageStatus) {
-          this.BackPageStatus[i] = false;
-        }
-
-        this.ocaside();
-
-        this.BackPageStatus[name] = true;
-      },
-
-      CalTime (time) {
-        let word = '';
-
-        if (time.hour != 0) {
-          word += time.hour + 'h';
-        } else if (time.min != 0) {
-          word += time.min + 'h';
-        } else if (time.sec != 0) {
-          word += time.sec + 'h';
-        }
-
-        return word;
-      },
-
-      ocaside () {
-        this.AsideStatus = !this.AsideStatus;
-
-        if (this.AsideStatus) {
-          this.AsidePos = '0%';
-        } else {
-          this.AsidePos = '-9%';
-        }
-      },
-
-      OCAddOrder () {
-        this.AddOrderStatus = !this.AddOrderStatus;
-
-        if (this.AddOrderStatus) {
-          for (let i in this.ordering.data) {
-            this.ordering.data[i].count = 0;
-          }
-        }
-      },
-
-      OCDetail () {
-        this.DetailStatus = !this.DetailStatus;
-      },
-
-      remove (order) {
-        this.CashOdishData.splice(order,1);
-      },
-
-      start (order) {
-        alert(order);
-      },
-
-      stop (order) {
-        alert(order);
-      },
-
-      finish (order) {
-        this.MdishData[this.MdishType].splice(order,1);
+      } else {
+        HeaderBtnStatus.value[userinfo.value.account + 'btn'] = true;
       }
+    } else {
+      WrongLogin.value = true;
     }
   }
+
+  function ChangePage (name) {
+    for (let i in MainPageStatus.value) {
+      MainPageStatus.value[i] = false;
+    }
+
+    if (AsideStatus.value) {
+      ocaside();
+    }
+    
+    MainPageStatus.value[name] = true;
+  }
+
+  function ChangeBackPage (name) {
+    for (let i in BackPageStatus.value) {
+      BackPageStatus.value[i] = false;
+    }
+    BackPageStatus.value[name] = true;
+
+    ocaside();
+  }
+
+  function CalTime (time) {
+    let word = '';
+
+    if (time.hour != 0) {
+      word += time.hour + 'h';
+    } else if (time.min != 0) {
+      word += time.min + 'h';
+    } else if (time.sec != 0) {
+      word += time.sec + 'h';
+    }
+
+    return word;
+  }
+
+  function ocaside () {
+    AsideStatus.value = !AsideStatus.value;
+
+    if (AsideStatus.value) {
+      AsidePos.value = '0%';
+    } else {
+      AsidePos.value = '-9%';
+    }
+  }
+
+  function remove (order) {
+    CashOdishData.value.splice(order,1);
+  }
+
+  function start (order) {
+    alert(order);
+  }
+
+  function stop (order) {
+    alert(order);
+  }
+
+  function finish (order) {
+    MdishData.value[MdishType].splice(order,1);
+  }
+
+  function OCAddOrder (show) {
+    if (!AddOrder.value) return;
+    if (show) {
+      AddOrder.value.showModal();
+    } else {
+      AddOrder.value.close();
+    }
+  }
+
+  function OCDetail (show) {
+    if (!Detail.value) return;
+    if (show) {
+      Detail.value.showModal();
+    } else {
+      Detail.value.close();
+    }
+  }
+
+  function detail (order) {
+    console.log(order);
+    DetailStatus.value = true;
+    OCDetail(true);
+  }
+
+  function AOHCOS ({ clientX: x, clientY: y }) {
+    if (!AddOrder.value) return;
+
+    const { left, right, top, bottom } = AddOrder.value.getBoundingClientRect();
+    if (x < left || x > right || y < top || y > bottom) {
+      AddOrderStatus.value = false;
+    }
+  }
+
+  function DHCOS ({ clientX: x, clientY: y }) {
+    if (!Detail.value) return;
+
+    const { left, right, top, bottom } = Detail.value.getBoundingClientRect();
+    if (x < left || x > right || y < top || y > bottom) {
+      DetailStatus.value = false;
+    }
+  }
+
+  onMounted(() => {
+    if (AddOrderStatus.value) OCAddOrder(true);
+    if (DetailStatus.value) OCDetail(true);
+  });
+
+  watch(AddOrderStatus, (isOpen) => {
+    if (isOpen) OCAddOrder(isOpen);
+  });
+
+  watch(DetailStatus, (isOpen) => {
+    if (isOpen) OCDetail(isOpen);
+  });
 </script>
 
 <style>
   aside {
     left: v-bind(AsidePos);
     transition: all 1s;
+  }
+
+  .dialog-enter-from,
+  .dialog-leave-to {
+    opacity: 0;
+  }
+
+  .dialog-enter-active {
+    transition-timing-function: "ease-out";
+  }
+
+  .dialog-leave-active {
+    transition-timing-function: "ease-in";
+  }
+
+  .dialog-enter-active,
+  .dialog-leave-active {
+    transition-duration: 200ms;
+  }
+
+  .dialog-enter-from#addorder,
+  .dialog-leave-to#addorder {
+    transform: scale(0.9) translateY(-2rem);
+  }
+
+  .dialog-enter-from#detail,
+  .dialog-leave-to#detail {
+    transform: scale(0.9) translateY(-2rem);
   }
 </style>
